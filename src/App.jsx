31fc916f956b53f050785c7917884cb952154a52
Erdal ŞAHAN <dhm23198@dhmi.root>
2025-10-28 11:07:10 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from "react";
+import React, {useState, useEffect} from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import FirstPage from "./components/FirstPage";
 import Game from "./components/Game";
